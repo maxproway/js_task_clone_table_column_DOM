9c9b968707e4f244de13f6b74e36cb00511c7f4d
Max <max@energyhill.com>
2024-07-31 16:17:34 -0400

    make build

---
+++ index.700444b3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=!0,l=!1,t=void 0;try{for(var n,a=e.rows[Symbol.iterator]();!(r=(n=a.next()).done);r=!0){var o=n.value,c=document.createElement(o.cells[1].tagName);c.innerHTML=o.cells[1].innerHTML,o.insertBefore(c,o.cells[o.cells.length-1])}}catch(e){l=!0,t=e}finally{try{r||null==a.return||a.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.700444b3.js.map
